5c5c1aaeeab6d4cf1695e75e05ce57a3f48e6d26
Tech-claudia <cdaniemuriel@gmail.com>
2021-05-02 11:23:59 -0500

    Updated current date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}: ${minutes}`;
+}
+
 function currentWeather(response) {
   let showCity = document.querySelector("#current-city");
   showCity.innerHTML = response.data.name;
@@ -15,6 +39,9 @@ function currentWeather(response) {
   let wind = Math.round(response.data.wind.speed);
   let windSpeed = document.querySelector("#wind");
   windSpeed.innerHTML = `Wind speed: ${wind}km/hr`;
+
+  let dateElement = document.querySelector("#current-date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 function searchCity(event) {

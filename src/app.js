22f40dbf8a2601a9f3b41d2c743c960cc3b54292
Tech-claudia <cdaniemuriel@gmail.com>
2021-05-15 21:45:06 -0500

    Changed icons size

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ function displayForecast(response) {
                   forecastDay.weather[0].icon
                 }@2x.png"
                 alt=""
-             width="30px"
+             width="40px"
                 id="icon"
               />
               <div class="forecast-temp">
